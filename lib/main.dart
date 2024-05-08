f34825dd5be4d8df39380d06870e26c839ed5937
Krystian Juszczyk <juszczyk-krystian@wp.pl>
2024-05-08 19:26:50 +0200

    File/Folder structure made.

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 78, 206, 28)),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromRGBO(242, 100, 50, 1)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'PLeasure'),
@@ -81,7 +81,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Colors.grey,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
