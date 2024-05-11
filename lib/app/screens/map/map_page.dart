f9c45afaa3675a9e4c3ccc4c544a309e4fd59fb5
Krystian Juszczyk <juszczyk-krystian@wp.pl>
2024-05-11 17:32:04 +0200

    Changed system dependencies.

--- lib/app/screens/map/map_page.dart
+++ lib/app/screens/map/map_page.dart
@@ -7,8 +7,8 @@ class MapPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const BaseView(
-      body: Center(
-        child: Text('Map goes here'),
+      body: Scaffold(
+
       )
     );
   }
