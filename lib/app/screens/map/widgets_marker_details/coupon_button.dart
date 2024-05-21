64e135b57a9338b118c72214987a41f338249627
Krystian Juszczyk <juszczyk-krystian@wp.pl>
2024-05-21 23:49:33 +0200

    State of reclaiming coupon.

--- lib/app/screens/map/widgets_marker_details/coupon_button.dart
+++ lib/app/screens/map/widgets_marker_details/coupon_button.dart
@@ -2,44 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:inner_shadow_widget/inner_shadow_widget.dart';
 import '/app/shared/themes/theme.dart';
 
-class CouponButton extends StatelessWidget {
-  const CouponButton({super.key});
+class CouponButton extends StatefulWidget {
+  const CouponButton({Key? key}) : super(key: key);
+
+  @override
+  _CouponButtonState createState() => _CouponButtonState();
+}
+
+class _CouponButtonState extends State<CouponButton> {
+  bool _isClicked = false;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.35,
+      width: MediaQuery.of(context).size.width * 0.34,
       height: MediaQuery.of(context).size.height * 0.09,
       child: FloatingActionButton(
-          backgroundColor: buttonColor,
-          onPressed: () {
-          },
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(60.0),
-          ),
-          elevation: 5,
-          child: const InnerShadow(
-            blur: 1,
-            color: Colors.black12,
-            offset: Offset(0, 2),
-            child: Center(
-              child: Text(
-                'Odbierz',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.white,
-                  shadows: [
-                    Shadow(
-                      color: Colors.black12,
-                      blurRadius: 2,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
-                ),
+        backgroundColor: _isClicked ? secondaryColor : buttonColor,
+        onPressed: () {
+          setState(() {
+            _isClicked = true;
+          });
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(60.0),
+        ),
+        elevation: 5,
+        child: InnerShadow(
+          blur: 1,
+          color: Colors.black12,
+          offset: const Offset(0, 2),
+          child: Center(
+            child: Text(
+              _isClicked ? 'Odebrano' : 'Odbierz',
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+                shadows: [
+                  Shadow(
+                    color: Colors.black12,
+                    blurRadius: 2,
+                    offset: Offset(0, 3),
+                  ),
+                ],
               ),
             ),
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
